c3f9f4cd5a8d9321852bfc923c4bf8d672959d30
Austin Rochford <austin.rochford@gmail.com>
2013-02-10 16:47:10 -0600

    Added forward algorithm

--- forward_backward.py
+++ forward_backward.py
@@ -1,5 +1,16 @@
 from numpy import diagflat
 
+#forward-backward algorithm
+def forward(transition_probs, emission_probs, initial_dist, emissions):
+    dists = [init]
+    dist = init
+
+    for emission in emissions:
+        dists.append(dot(dist, dot(transition_probs, emission_marix(emission_probs, emissions))))
+
+    return dists
+
+#distribution manipulation utilities
 def emission_matrix(emission_probs, emission):
     return diagflat(emission_probs[:, emission])
 
