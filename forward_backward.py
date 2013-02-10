21931a3398b1c999e0d897c858b8948d99c868b3
Austin Rochford <austin.rochford@gmail.com>
2013-02-10 17:25:10 -0600

    Added backward algorithm, passes test

--- forward_backward.py
+++ forward_backward.py
@@ -1,9 +1,19 @@
-from numpy import array, diagflat, dot
+from numpy import array, diagflat, dot, transpose
 
 #forward-backward algorithm
+def backward(transition_probs, emission_probs, emissions):
+    dist = to_dist(array([[1.0] * emission_probs.shape[0]]))
+    dists = [dist]
+
+    for emission in reversed(emissions):
+        dist = transpose(to_dist(dot(transition_probs, dot(emission_matrix(emission_probs, emission), transpose(dist)))))
+        dists.append(dist)
+
+    return dists
+
 def forward(transition_probs, emission_probs, initial_dist, emissions):
-    dists = [initial_dist]
     dist = initial_dist
+    dists = [initial_dist]
 
     for emission in emissions:
         dist = to_dist(dot(dist, dot(transition_probs, emission_matrix(emission_probs, emission))))
@@ -16,7 +26,7 @@ def emission_matrix(emission_probs, emission):
     return diagflat(emission_probs[:, emission])
 
 def to_dist(v):
-    return (1 / sum(abs(v.flatten()))) * v
+    return v / sum(abs(v.flatten()))
 
 #examples
 #from wikipedia
@@ -26,4 +36,5 @@ wiki_emissions = [0, 0, 1, 0, 0]
 wiki_transition_probs = array([[0.7, 0.3], [0.3, 0.7]])
 
 if __name__ == "__main__":
-    print forward(wiki_transition_probs, wiki_emission_probs, wiki_initial_dist, wiki_emissions)
+    #print forward(wiki_transition_probs, wiki_emission_probs, wiki_initial_dist, wiki_emissions)
+    print backward(wiki_transition_probs, wiki_emission_probs, wiki_emissions)
