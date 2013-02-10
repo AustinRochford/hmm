fcc22f520a1a4940d28252bcd6f0b0527a6ef9c8
Austin Rochford <austin.rochford@gmail.com>
2013-02-10 16:51:03 -0600

    Added wikipedia example and test code

--- forward_backward.py
+++ forward_backward.py
@@ -2,8 +2,8 @@ from numpy import diagflat
 
 #forward-backward algorithm
 def forward(transition_probs, emission_probs, initial_dist, emissions):
-    dists = [init]
-    dist = init
+    dists = [initial_dist]
+    dist = initial_dist
 
     for emission in emissions:
         dists.append(dot(dist, dot(transition_probs, emission_marix(emission_probs, emissions))))
@@ -16,3 +16,13 @@ def emission_matrix(emission_probs, emission):
 
 def to_dist(v):
     return (1 / sum(abs(v.flatten()))) * v
+
+#examples
+#from wikipedia
+wiki_emission_probs = array([[0.9, 0.1], [0.2, 0.8]])
+wiki_initial_dist = array([[0.5, 0.5]])
+wiki_emissions = [0, 0, 1, 0, 0]
+wiki_transition_probs = array([[0.7, 0.3], [0.3, 0.7]])
+
+if __name__ == "__main__":
+    print forward(wiki_transition_probs, wiki_emission_probs, wiki_initial_dist, wiki_states)
