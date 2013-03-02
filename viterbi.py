3b8f8590c0695e6e42bf6989a995b863c4828f4c
Austin Rochford <austin.rochford@gmail.com>
2013-03-02 23:28:03 +0000

    Coded forward pass

--- viterbi.py
+++ viterbi.py
@@ -3,8 +3,11 @@ from hmm import HMM
 def viterbi(hmm, initial_dist, emissions):
     #it would be nice not to have to treat the initial emission differently
     ix_stack = []
-    last_probs = hmm.emission_dist(emissions[0]) * initial_dist
-    prob_stack = [last_probs]
+    prob_stack = [hmm.emission_dist(emissions[0]) * initial_dist]
 
     for emission in emissions[1:]:
-        max_ixs = np.argmax(hmm.transition_probs() * last_probs,
+        last_probs = hmm.transition_probs() * prob_stack[-1]
+        max_ixs = np.argmax(last_probs, axis=0)
+        ix_stack.append(max_ixs)
+
+        prob_stack.append(hmm.emission_dist(emission) * last_probs[max_ixs, np.arange(hmm.num_states())])
