8a803fea198d413f611b41e918fcb3201bc632a5
Austin Rochford <austin.rochford@gmail.com>
2013-03-02 21:42:34 +0000

    Basic HMM class done

--- hmm.py
+++ hmm.py
@@ -6,6 +6,9 @@ class HMM:
         self._transition_probs = transition_probs
         self._emission_probs = emission_probs
 
-    #accessor
-    def emission_dist(emission):
+    #accessors
+    def emission_dist(self, emission):
         return self._emission_probs[:, emission]
+
+    def transition_probs(self):
+        return self._transition_probs
