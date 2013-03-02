ef4dd17341d90246c2b04fb05cfc1bf402342dc3
Austin Rochford <austin.rochford@gmail.com>
2013-03-02 17:18:36 +0000

    Changed dists from matrices to vectors

--- forward_backward.py
+++ forward_backward.py
@@ -1,3 +1,4 @@
+import itertools as itl
 import numpy as np
 
 #forward-backward algorithm
@@ -10,6 +11,12 @@ def backward(transition_probs, emission_probs, emissions):
         dists.insert(0, dist)
 
     return dists
+
+def forward_backward(transition_probs, emission_probs, initial_dist, emissions):
+    forward_dists = forward(transition_probs, emission_probs, initial_dist, emissions)
+    backward_dists = backward(transition_probs, emission_probs, emissions)
+
+    return np.multiply(forward_dists, backward_dists)
 
 def forward(transition_probs, emission_probs, initial_dist, emissions):
     dist = initial_dist
@@ -26,22 +33,23 @@ def emission_dist(emission_probs, emission):
     return np.diagflat(emission_probs[:, emission])
 
 def l1norm(array_):
+    #this is odd; why is it inf? to get l1?
     return np.linalg.norm(array_, np.inf)
 
 def normalize(array_):
     return array_ / l1norm(array_)
 
 def uniform(n):
-    return normalize(np.ones((1,n)))
+    return normalize(np.ones(n))
 
 #examples
 #from wikipedia
 wiki_emission_probs = np.array([[0.9, 0.1], [0.2, 0.8]])
-wiki_initial_dist = np.array([[0.5, 0.5]])
+wiki_initial_dist = np.array([0.5, 0.5])
 wiki_emissions = [0, 0, 1, 0, 0]
 wiki_transition_probs = np.array([[0.7, 0.3], [0.3, 0.7]])
 
 if __name__ == "__main__":
-    #print(forward(wiki_transition_probs, wiki_emission_probs, wiki_initial_dist, wiki_emissions))
+    print(forward(wiki_transition_probs, wiki_emission_probs, wiki_initial_dist, wiki_emissions))
     print(backward(wiki_transition_probs, wiki_emission_probs, wiki_emissions))
     #print(forward_backward(wiki_transition_probs, wiki_emission_probs, wiki_initial_dist, wiki_emissions))
