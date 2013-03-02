831c4aa494ab51e123dbac5506d47f7bb697aa3b
Austin Rochford <austin.rochford@gmail.com>
2013-03-02 23:50:22 +0000

    A first try

--- viterbi.py
+++ viterbi.py
@@ -1,13 +1,24 @@
 from hmm import HMM
 
+#the viterbi algorithm
 def viterbi(hmm, initial_dist, emissions):
-    #it would be nice not to have to treat the initial emission differently
+    #it would be nice not to have to treat the initial emission differently, is it possible?
     ix_stack = []
-    prob_stack = [hmm.emission_dist(emissions[0]) * initial_dist]
+    last_probs = hmm.emission_dist(emissions[0]) * initial_dist
 
     for emission in emissions[1:]:
-        last_probs = hmm.transition_probs() * prob_stack[-1]
+        probs = hmm.transition_probs() * last_probs
         max_ixs = np.argmax(last_probs, axis=0)
         ix_stack.append(max_ixs)
 
-        prob_stack.append(hmm.emission_dist(emission) * last_probs[max_ixs, np.arange(hmm.num_states())])
+        last_probs = hmm.emission_prob(emission) * probs[max_ixs, np.arange(hmm.num_states())]
+
+    states = [np.argmax(last_probs)]
+
+    while ix_stack:
+        max_ixs = ix_stack.pop()
+        states.append(max_ixs[states[-1]])
+
+    states.reverse()
+
+    return states
