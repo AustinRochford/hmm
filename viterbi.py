354a7f74cfff3d0287857a80c644f8be754dfbc4
Austin Rochford <austin.rochford@gmail.com>
2013-03-03 16:28:20 +0000

    It works

--- viterbi.py
+++ viterbi.py
@@ -3,13 +3,32 @@ import numpy as np
 
 #the Viterbi algorithm
 def viterbi(hmm, initial_dist, emissions):
-    pass
+    probs = hmm.emission_dist(emissions[0]) * initial_dist
+    stack = []
+
+    for emission in emissions[1:]:
+        trans_probs = hmm.transition_probs * np.row_stack(probs)
+        max_row_ixs = np.argmax(trans_probs, axis=0)
+        probs = hmm.emission_dist(emission) * trans_probs[max_row_ixs, np.arange(hmm.num_states)]
+
+        stack.append(max_row_ixs)
+
+    state_seq = [np.argmax(probs)]
+
+    while stack:
+        max_row_ixs = stack.pop()
+        state_seq.append(max_row_ixs[state_seq[-1]])
+
+    state_seq.reverse()
+
+    return state_seq
+
 
 #examples
 #from Wikipedia
 wiki_transition_probs = np.array([[0.7, 0.4], [0.3, 0.6]]) #0=Healthy, 1=Fever
 wiki_emissions = [2, 1, 0]
-wiki_emission_probs = np.array([[0.1, 0.6], [0.4, 0.3], [0.5, 0.1]]).T #0=Dizzy, 1=Cold, 2=Normal
+wiki_emission_probs = np.array([[0.1, 0.4, 0.5], [0.6, 0.3, 0.1]]) #0=Dizzy, 1=Cold, 2=Normal
 wiki_initial_dist = np.array([[0.6, 0.4]])
 wiki_hmm = HMM(wiki_transition_probs, wiki_emission_probs)
 
