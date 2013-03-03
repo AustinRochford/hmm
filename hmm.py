6a646c31f5c83cbf5ef5bba23e87fae4cdb3e4b1
Austin Rochford <austin.rochford@gmail.com>
2013-03-03 16:33:35 +0000

    Documented HMM constructor

--- hmm.py
+++ hmm.py
@@ -1,7 +1,7 @@
-import numpy as np
-
 class HMM:
     #constructor
+    #transition_probs[i, j] is the probability of transitioning to state i from state j
+    #emission_probs[i, j] is the probability of emitting emission j while in state i
     def __init__(self, transition_probs, emission_probs):
         self._transition_probs = transition_probs
         self._emission_probs = emission_probs
