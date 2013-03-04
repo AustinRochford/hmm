93ab046b6479742503f924cd1bf0adb1ddef07c7
Austin Rochford <austin.rochford@gmail.com>
2013-03-04 23:54:52 +0000

    More HMM utilities

--- haskell/hmm.hs
+++ haskell/hmm.hs
@@ -1,11 +1,17 @@
-import Data.Packed.Matrix (Matrix, rows)
+import Data.Packed.Matrix
 import Data.Packed.Vector (Vector)
 
 --HMM types
 type Dist = Vector Prob
-data HMM = HMM {transProbs :: Matrix Prob, emitProbs :: Matrix Prob}
+type Emission = Int
 type Prob = Double
+type State = Int
+
+data HMM = HMM {transProbs :: Matrix Prob, emitProbs :: Matrix Prob}
 
 --basic utilities
+emissDist :: HMM -> Emission -> Dist
+emissDist hmm emiss = flatten $ subMatrix (0, emiss) (numStates hmm, 1) $ emitProbs hmm
+
 numStates :: HMM -> Int
 numStates = rows . transProbs
