68177cb101eba0416d1d2c3ca29e33552d779f17
Austin Rochford <austin.rochford@gmail.com>
2013-03-04 23:06:12 +0000

    Basic HMM type and utilities

--- haskell/hmm.hs
+++ haskell/hmm.hs
@@ -1,1 +1,11 @@
-import hmatrix
+import Data.Packed.Matrix (Matrix, rows)
+import Data.Packed.Vector (Vector)
+
+--HMM types
+type Dist = Vector Prob
+data HMM = HMM {transProbs :: Matrix Prob, emitProbs :: Matrix Prob}
+type Prob = Double
+
+--basic utilities
+numStates :: HMM -> Int
+numStates = rows . transProbs
