7ea1c896bb42951d7b327aa0014a8c87578af791
Austin Rochford <austin.rochford@gmail.com>
2013-03-05 00:00:12 +0000

    Added f-b example from wiki

--- haskell/hmm.hs
+++ haskell/hmm.hs
@@ -1,5 +1,5 @@
 import Data.Packed.Matrix
-import Data.Packed.Vector (Vector)
+import Data.Packed.Vector
 
 --HMM types
 type Dist = Vector Prob
@@ -10,8 +10,15 @@ type State = Int
 data HMM = HMM {transProbs :: Matrix Prob, emitProbs :: Matrix Prob}
 
 --basic utilities
-emissDist :: HMM -> Emission -> Dist
+--this isn't the best name to represent it think more about what it represents
+emissDist:: HMM -> Emission -> Dist
 emissDist hmm emiss = flatten $ subMatrix (0, emiss) (numStates hmm, 1) $ emitProbs hmm
 
 numStates :: HMM -> Int
 numStates = rows . transProbs
+
+--examples
+wikiFBEmitProbs = (2><2) [0.9, 0.1, 0.2, 0.8]
+wikiFBInitProbs = (1><2) [0.5, 0.5]
+wikiFBTransProbs = (2><2) [0.7, 0.3, 0.3, 0.7]
+wikiFBHMM = HMM wikiFBTransProbs wikiFBEmitProbs
