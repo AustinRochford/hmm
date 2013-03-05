7b3a3749d6d4795539ab7c4a10cce25ddf643b8d
Austin Rochford <austin.rochford@gmail.com>
2013-03-05 00:41:23 +0000

    Added normalize

--- haskell/hmm.hs
+++ haskell/hmm.hs
@@ -1,5 +1,7 @@
+module HMM where
 import Data.Packed.Matrix
 import Data.Packed.Vector
+import Numeric.Container
 
 --HMM types
 type Dist = Vector Prob
@@ -14,11 +16,14 @@ data HMM = HMM {transProbs :: Matrix Prob, emitProbs :: Matrix Prob}
 emissDist:: HMM -> Emission -> Dist
 emissDist hmm emiss = flatten $ subMatrix (0, emiss) (numStates hmm, 1) $ emitProbs hmm
 
+normalize :: Vector Double -> Dist
+normalize v = scale (1 / sumElements v) v
+
 numStates :: HMM -> Int
 numStates = rows . transProbs
 
 --examples
 wikiFBEmitProbs = (2><2) [0.9, 0.1, 0.2, 0.8]
-wikiFBInitProbs = (1><2) [0.5, 0.5]
+wikiFBInitProbs = (2|>) [0.5, 0.5] :: Dist
 wikiFBTransProbs = (2><2) [0.7, 0.3, 0.3, 0.7]
 wikiFBHMM = HMM wikiFBTransProbs wikiFBEmitProbs
