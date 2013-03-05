12e8a644077158a62a681d50207da71786efbf72
Austin Rochford <austin.rochford@gmail.com>
2013-03-05 16:18:16 +0000

    Added Viterbi example from wiki

--- haskell/HMM.hs
+++ haskell/HMM.hs
@@ -26,8 +26,16 @@ uniform :: Int -> Dist
 uniform n = normalize . (n |>) $ repeat 1
 
 --examples
+--forward-backward algorithm
 wikiFBEmissions = [0, 0, 1, 0, 0] :: [Emission]
 wikiFBEmitProbs = (2><2) [0.9, 0.1, 0.2, 0.8]
-wikiFBInitProbs = (2|>) [0.5, 0.5] :: Dist
+wikiFBInitProbs = 2 |> [0.5, 0.5] :: Dist
 wikiFBTransProbs = (2><2) [0.7, 0.3, 0.3, 0.7]
 wikiFBHMM = HMM wikiFBTransProbs wikiFBEmitProbs
+
+--viterbi algorithm
+wikiVitEmissions = [2, 1, 0] :: [Emission] --0=dizzy, 1=cold, 2=normal
+wikiVitEmitProbs = (2><3) [0.1, 0.4, 0.5, 0.6, 0.3, 0.1]
+wikiVitInitProbs = 2 |> [0.6, 0.4] :: Dist
+wikiVitTransProbs = (2><2) [0.7, 0.4, 0.3, 0.6] --0=healthy, 1=fever
+wikiVitHMM = HMM wikiVitTransProbs wikiVitEmitProbs
