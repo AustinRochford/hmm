19b0566a56146309d6a7268efead37e86099f7c6
Austin Rochford <austin.rochford@gmail.com>
2013-03-05 01:48:34 +0000

    It works

--- haskell/fb.hs
+++ haskell/fb.hs
@@ -16,3 +16,7 @@ forward hmm = scanl (forwardStep hmm)
 
 forwardStep :: HMM -> Dist -> Emission -> Dist
 forwardStep hmm dist emiss = normalize $ dist <> (transProbs hmm) <> (diag $ emissDist hmm emiss)
+
+--forward-backward algorithm
+fb :: HMM -> Dist -> [Emission] -> [Dist]
+fb hmm dist emissions = zipWith (curry (normalize . (uncurry mul))) (forward hmm dist emissions) (backward hmm emissions)
